5a0dca21a9ca18da5b660ac09ba53eea4a8e3d08
Taylor Smith <toss45@gmail.com>
2021-09-19 06:28:45 -0700

    finished templating tags & styling

--- black_jack_project/black_jack/views.py
+++ black_jack_project/black_jack/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, redirect
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	context = {}
+	template = loader.get_template('index_modified.html')
+	return HttpResponse(template.render(context, request))
+
+def html(request):
+    pass 
+'''
+    context = {}
+    # The template to be loaded as per gentelella.
+    # All resource paths for gentelella end in .html.
+
+    # Pick out the html file name from the url. And load that template.
+    load_template = request.path.split('/')[-1]
+    template = loader.get_template('app/' + load_template)
+    return HttpResponse(template.render(context, request)) '''
