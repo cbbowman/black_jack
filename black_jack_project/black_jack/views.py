8b88274525d1cb1207517aaf85c928ff1e1d3e34
Jorge Obeso <j.obeso.dev@gmail.com>
2021-09-20 02:23:01 -0700

    User Login and Authentication

--- black_jack_project/black_jack/views.py
+++ black_jack_project/black_jack/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        User.objects.create_user(username = request.POST['username'],first_name=request.POST['first_name'], last_name=request.POST['last_name'],email=request.POST['email'], password=request.POST['password'])
+        print(request)
+        return redirect('/')
+    return redirect('/')
+
+def login_user(request):
+    if request.method == 'POST':
+        logged_user =authenticate(username=request.POST['username'],password=request.POST['password'])
+        login(request,logged_user)
+        return redirect('/')
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
--- black_jack_project/black_jack_project/settings.py
+++ black_jack_project/black_jack_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'black_jack',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
