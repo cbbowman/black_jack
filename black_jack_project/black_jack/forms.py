6f1df1da79f17e691c022b4bc082f2377b27ccd9
Taylor Smith <toss45@gmail.com>
2021-09-22 20:07:37 -0700

    increased email field size,placed game.html in content block,  & updated .gitignore

--- black_jack_project/black_jack/forms.py
+++ black_jack_project/black_jack/forms.py
@@ -4,11 +4,10 @@ class RegisterForm(forms.Form):
     username = forms.CharField(max_length=20)
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
-    email = forms.CharField(max_length=20,widget= forms.EmailInput)
+    email = forms.CharField(max_length=30,widget= forms.EmailInput)
     password = forms.CharField(max_length=20,widget= forms.PasswordInput)
     confirm = forms.CharField(max_length=20,widget= forms.PasswordInput)
 
-    
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
